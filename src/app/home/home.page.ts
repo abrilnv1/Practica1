94788ba9d231139762534c45b961377e9ad238da
CarolinaCastH <CarolinaCastH.16@gmail.com>
2023-10-18 23:23:26 -0600

    Solución al bug al agregar nuevas Tareas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
+import { CreateTaskUseCase } from './../core/tasks/use-cases/create-task.use-case';
+import { GetTaskUseCase } from './../core/tasks/use-cases/get-task.use-case';
 import { Component, ViewChild } from '@angular/core';
-import { IonModal, ModalController } from '@ionic/angular';
-import { GetTaskUseCase } from "./../core/tasks/use-cases/get-task.use-case";
-import { CreateTaskUseCase } from "./../core/tasks/use-cases/create-task.use-case";
+import { IonModal } from '@ionic/angular';
 
-interface tarea {
+
+interface tarea{
   id: string;
-  nombre: string,
-  descripcion: string,
+  nombre: string;
+  descripcion: string;
   prioridad: string;
 }
 
@@ -16,61 +17,61 @@ interface tarea {
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  @ViewChild(IonModal)modal!: IonModal;
+
+  @ViewChild(IonModal) modal!: IonModal;
   tareas: tarea[] = [];
 
   tareaEditada: tarea | null = null;
+
+
   nombre: string = "";
   descripcion: string = "";
   prioridad: string = "";
   
+
   constructor(
-    private ModalController: ModalController,
     private getTaskUseCase: GetTaskUseCase,
-    private createTaskUseCase : CreateTaskUseCase,
+    private createTaskUseCase: CreateTaskUseCase,
   ) {}
 
   async ngOnInit(){
     this.tareas = (await this.getTaskUseCase.execute());
   }
-  
 
-  cancel() {
+  cancel(){
     this.modal.dismiss(null, 'cancel');
   }
 
   async confirm() {
+      if (this.tareaEditada){
+        this.tareaEditada.nombre = this.nombre;
+        this.tareaEditada.descripcion = this.descripcion;
+        this.tareaEditada.prioridad = this.prioridad;
+      }else{
+        await this.createTaskUseCase.execute(this.nombre, this.descripcion, this.prioridad);
+        this.tareas = (await this.getTaskUseCase.execute());
+      }
 
-    if (this.tareaEditada) {
-      this.tareaEditada.nombre = this.nombre;
-      this.tareaEditada.descripcion = this.descripcion;
-      this.tareaEditada.prioridad = this.prioridad;
-    }
-    else {
-      await this.createTaskUseCase.execute(this.nombre, this.descripcion, this.prioridad);
-      this.tareas = (await this.getTaskUseCase.execute());
-    }
-
-    //LIMPIAR LOS DATOS
+    
     this.nombre = "";
-    this.descripcion= "";
-    this.prioridad="";
-    this.modal.dismiss(null, 'confirm');
+    this.descripcion = "";
+    this.prioridad = "";
+    this.tareaEditada = null;
+    this.modal.dismiss(this.modal, 'confirm');
   }
 
-  eliminarTarea(item: any) {
-    const index = this.tareas.indexOf(item)
+
+  eliminarTarea(item: any){
+    const index = this.tareas.indexOf(item);
     this.tareas.splice(index, 1);
   }
 
-
-  editarTarea(item: tarea) {
+  editarTareas(item: tarea){
     this.tareaEditada = item;
     this.nombre = item.nombre;
     this.descripcion = item.descripcion;
     this.prioridad = item.prioridad;
     this.modal.present();
-   
   }
 
 
